3ac14e7ed1fca9806bfa1abb2d7fe96525265674
YensyGomez <yensy@sirius.utp.edu.co>
2018-04-20 18:03:06 -0500

    modify hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
-  render html: "hello, world!"
+  def hello
+    render html: "hello, world!"
+  end
 end
